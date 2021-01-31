ddf44b80b50a463dc3fcacc4b0dd809ab60d732e
Brian <bbbrianboylen@gmail.com>
2021-01-31 13:51:06 -0600

    created layout for app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import Header from "./components/header";
+import Info from "./components/info";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="page-wrapper">
+      <header class="header">
+        <Header />
       </header>
+      <main>
+        <Info />
+      </main>
     </div>
   );
 }
---
+++ src/components/info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/info.css';
+
+function Info() {
+  return (
+    <div class="personal-info">
+      <h2>Personal Info</h2>
+      <form class="info-form">
+        
+      </form>
+    </div>
+  )
+}
+
+export default Info
