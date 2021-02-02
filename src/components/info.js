1c8babc859ed979567344097cd4daf6156d8a1df
Brian <bbbrianboylen@gmail.com>
2021-02-01 19:50:44 -0600

    adding form behavior

--- src/components/info.js
+++ src/components/info.js
@@ -4,31 +4,87 @@ import "../styles/info.css";
 class Info extends React.Component {
   constructor(props) {
     super();
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: "",
+      formSubmitted: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  handleChange(e) {
+    switch (e.target.attributes.getNamedItem("name").nodeValue) {
+      case "first-name":
+        this.setState({ firstName: e.target.value })
+        break;
+      case "last-name":
+        this.setState({ lastName: e.target.value })
+        break;
+      case "email":
+        this.setState({ email: e.target.value })
+        break;
+      case "phone-number":
+        this.setState({ phoneNumber: e.target.value })
+        break;
+      default:
+    }
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    // validate form
+    this.setState({ formSubmitted: true});
   }
 
   render() {
     return (
       <div className="personal-info">
         <h2>Personal Info</h2>
-        <form className="info-form">
+        {!this.state.formSubmitted ? (
+        <form className="info-form" onSubmit={this.submitForm}>
           <div className="input-field">
             <label htmlFor="first-name">First Name</label>
-            <input type="text" name="first-name"></input>
+            <input
+              type="text"
+              name="first-name"
+              onChange={this.handleChange}
+            ></input>
           </div>
           <div className="input-field">
             <label htmlFor="last-name">Last Name</label>
-            <input type="text" name="last-name"></input>
+            <input
+              type="text"
+              name="last-name"
+              onChange={this.handleChange}
+            ></input>
           </div>
           <div className="input-field">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email"></input>
+            <input
+              type="email"
+              name="email"
+              onChange={this.handleChange}
+            ></input>
           </div>
           <div className="input-field">
             <label htmlFor="phone-number">Phone Number</label>
-            <input type="tel" name="phone-number"></input>
+            <input
+              type="tel"
+              name="phone-number"
+              onChange={this.handleChange}
+            ></input>
           </div>
-          <button type="submit" id="info-submit">Submit Info</button>
-        </form>
+          <button type="submit" id="info-submit">
+            Submit Info
+          </button>
+        </form>) : (
+          <div></div>
+        )}
       </div>
     );
   }
